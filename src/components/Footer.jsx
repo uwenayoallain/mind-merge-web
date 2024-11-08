f97d9d9e2eaa74303bdf662f7591ab7da6a9298a
uwenayoallain <uwenayoallain@gmail.com>
2024-11-08 14:47:41 +0200

    updated package manager

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -61,9 +61,7 @@ const Footer = () => {
         </div>
         <div className="mb-20 mt-24 flex flex-wrap items-end justify-between gap-x-6 gap-y-4 border-t border-neutral-950/10 pt-12">
           <Link href={"/"} aria-label="Home">
-            <Logo className="h-8">
-              Mind-Merge
-            </Logo>
+            <Logo className="h-8">Mind-Merge</Logo>
           </Link>
           <p className="text-sm text-neutral-700">
             © Mind-Merge. {new Date().getFullYear()} | Email:
